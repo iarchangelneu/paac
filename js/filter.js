b27cbe3e0a8184cac22e164feb90a4785eaf7b73
Valera <Milchakov.V@neutron-agency.com>
2023-09-17 15:13:23 +0300

    test

--- js/filter.js
+++ js/filter.js
@@ -1,18 +1,18 @@
-// $(document).ready(function () {
-//     if (document.getElementById("portfolio")) {
-//         var $grid = $(".grid").isotope({
-//             itemSelector: ".all",
-//             percentPosition: true,
-//             masonry: {
-//                 columnWidth: ".all"
-//             }
-//         });
+$(window).load(function(){
+    if (document.getElementById("portfolio")) {
+        var $grid = $(".grid").isotope({
+            itemSelector: ".all",
+            percentPosition: true,
+            masonry: {
+                columnWidth: ".all"
+            }
+        });
 
-//         $('.filters ul li').click(function () {
-//             $('.filters ul li').removeClass('active');
-//             $(this).addClass('active');
-//             var data = $(this).attr('data-filter');
-//             $grid.isotope({ filter: data });
-//         });
-//     }
-// });
+        $('.filters ul li').click(function () {
+            $('.filters ul li').removeClass('active');
+            $(this).addClass('active');
+            var data = $(this).attr('data-filter');
+            $grid.isotope({ filter: data });
+        });
+    }
+});
