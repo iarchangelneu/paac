2bc16e196da820cf81598e5fb6482e4bc49ab85a
Valera <Milchakov.V@neutron-agency.com>
2023-09-17 15:08:57 +0300

    eshe raz

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,18 @@
-     $('.filters ul li').click(function(){       
-        $('.filters ul li').removeClass('active');        
-        $(this).addClass('active');     
-        var data = $(this).attr('data-filter');       
-        $grid.isotope({filter: data})
-    });    
-        
-        
-        if(document.getElementById("portfolio"))
-        {     
-            var $grid = $(".grid").isotope({itemSelector:".all",
-                percentPosition: true,
-                masonry: {
-                    columnWidth:".all"
-                        }
-                    })
-        }
+$(document).ready(function () {
+    if (document.getElementById("portfolio")) {
+        var $grid = $(".grid").isotope({
+            itemSelector: ".all",
+            percentPosition: true,
+            masonry: {
+                columnWidth: ".all"
+            }
+        });
+
+        $('.filters ul li').click(function () {
+            $('.filters ul li').removeClass('active');
+            $(this).addClass('active');
+            var data = $(this).attr('data-filter');
+            $grid.isotope({ filter: data });
+        });
+    }
+});
